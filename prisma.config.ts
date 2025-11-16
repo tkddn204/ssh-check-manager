ab647f85128e4b687c5b968553d5587734314182
rightpair <tkddn204@gmail.com>
2025-11-16 23:37:29 +0900

    resolve env conflict from prisma

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+// Source - https://stackoverflow.com/a/79755797
+// Posted by DaanDeReus, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-16, License - CC BY-SA 4.0
+// import your .env file
+import { config } from 'dotenv';
+
+config({ path: '.env' })
